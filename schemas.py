8e2a8b82eb6bbd4fec143f688c56d46b01866f71
sjolivas <sjolivas@gmail.com>
2022-02-09 14:57:19 -0600

    Created Token

--- schemas.py
+++ schemas.py
@@ -215,3 +215,12 @@ class UserUpdate(BaseModel):
 class Login(BaseModel):
     username: str
     password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
