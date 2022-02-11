a8e2ef1c8904b8671c199cdd88942794f0c982a5
sjolivas <sjolivas@gmail.com>
2022-02-11 13:53:38 -0600

    origins

--- main.py
+++ main.py
@@ -32,8 +32,7 @@ app.include_router(item.router)
 # Make the connection to our frontend
 origins = [
     "https://localhost:3000",
-    "https://localhost:3000",
-    "http://localhost",
+    "http://localhost:3000",
     "http://localhost:8080",
 ]
 app.add_middleware(
